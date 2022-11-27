8c67554a37fa73cd51d5d79db88cafcce578bb80
Shahjhan-2k19 <56908544+Shahjhan-2k19@users.noreply.github.com>
2022-11-27 13:43:41 +0000

    Update index.js
    count down updates each sec

--- problem 2/index.js
+++ problem 2/index.js
@@ -1,13 +1,21 @@
 var startbtn = document.getElementById("start");
 rxjs.fromEvent(startbtn, "click").subscribe(() => time());
-function start() {
-  var time = rxjs.timer(1000);
-  var value = time.subscribe((val) => console.log(val));
+function start(time) {
+  var timer = rxjs.timer(time * 1000);
+  var stoptime = new Date().getTime() + time * 1000;//To Countdown every second
+  var x = setInterval(function () {
+    var now = new Date().getTime();
+    var distance = stoptime - now;
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("display").innerHTML = "Refresh";
+    }
+  }, 1000);
 }
 function time() {
   var h = document.getElementById("hours").value;
   var m = document.getElementById("minutes").value;
   var s = document.getElementById("seconds").value;
   time = h * 3600 + m * 60 + s;
-  console.log(time);
+  start(time);
 }
