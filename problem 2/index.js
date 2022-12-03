49e56823730752f1f115081b1bd8c5716bb540c8
Shahjhan-2k19 <56908544+Shahjhan-2k19@users.noreply.github.com>
2022-12-03 13:37:01 +0000

    Update index.js
    Using subscription interval which will get the difference in time every second and keep updating.

--- problem 2/index.js
+++ problem 2/index.js
@@ -1,21 +1,30 @@
 var startbtn = document.getElementById("start");
+// Using subscription interval which will get the difference in time every second and keep updating .
 rxjs.fromEvent(startbtn, "click").subscribe(() => time());
-function start(time) {
-  var timer = rxjs.timer(time * 1000);
-  var stoptime = new Date().getTime() + time * 1000;//To Countdown every second
-  var x = setInterval(function () {
+function start(time) { // Update with +1 count down to avoid delay,
+  // source https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_countdowe
+  var timer = rxjs.timer(time * 1000); 
+  var stoptime = new Date().getTime() + time * 1000 + 1000;
+  var x = setInterval(function () { 
     var now = new Date().getTime();
     var distance = stoptime - now;
-    if (distance < 0) {
+    // Calculations for hours, minutes and seconds using Math lib
+    var hours = Math.floor( (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60) ); 
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    document.getElementById("display").innerHTML = hours + " : "+ minutes + " : "+ seconds;// Display the result
+    if (distance < 0) { // If the count down over clear subscription interval 
       clearInterval(x);
-      document.getElementById("display").innerHTML = "Refresh";
-    }
-  }, 1000);
-}
-function time() {
-  var h = document.getElementById("hours").value;
-  var m = document.getElementById("minutes").value;
-  var s = document.getElementById("seconds").value;
-  time = h * 3600 + m * 60 + s;
-  start(time);
+      document.getElementById("display").innerHTML = "00 : 00";
+    }},
+    1000);
+     timer.subscribe(function(){ //Timer stops when hit 0 and reload
+     window.location.reload();
+  });}
+  function time() { 
+    var hour = document.getElementById("hours").value;
+    var min = document.getElementById("minutes").value;
+    var sec = document.getElementById("seconds").value;
+    time = hour * 3600 + min * 60 + sec;
+    start(time);
 }
